db437cf196f3e0a6cccdf5b43bf0edb2fdfc7b1d
ifqygazhar <ifqygazhar@gmail.com>
2024-12-03 09:11:58 +0700

    feat(info): add email send to for more support
    Add GitHub and Facebook buttons to the support page.
    Add Saweria button to the support page.
    Add a custom tab for email composer.

--- lib/fetures/info/page/info_page.dart
+++ lib/fetures/info/page/info_page.dart
@@ -30,6 +30,51 @@ class InfoPage extends StatelessWidget {
     }
   }
 
+  Future<void> _launchBrowser(
+      BuildContext context, String host, String path) async {
+    final Uri emailUri = Uri(scheme: 'https', host: host, path: path);
+
+    if (await canLaunchUrl(emailUri)) {
+      await launchUrl(
+        emailUri,
+        mode: LaunchMode.inAppWebView,
+        browserConfiguration: const BrowserConfiguration(showTitle: true),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Tidak dapat membuka email"),
+        ),
+      );
+    }
+  }
+
+  Future<void> _launchFb() async {
+    const String fbProtocolUrl = 'fb://page/61565055713502';
+
+    String fallbackUrl = 'https://www.facebook.com/Mangapp-61565055713502';
+
+    try {
+      bool launched = await launch(fbProtocolUrl, forceSafariVC: false);
+
+      if (!launched) {
+        await launch(fallbackUrl, forceSafariVC: false);
+      }
+    } catch (e) {
+      await launch(fallbackUrl, forceSafariVC: false);
+    }
+  }
+
+  Future<void> _launchChrome(String URI) async {
+    final String url = URI;
+
+    try {
+      await launch(url, forceSafariVC: false);
+    } catch (e) {
+      await launch(url, forceSafariVC: false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -97,27 +142,79 @@ class InfoPage extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            decoration: BoxDecoration(
-              color: ColorConstant.kThird,
-              borderRadius: BorderRadius.circular(8),
+          _buildTapSupportSocial(
+            context,
+            () => _launchBrowser(
+              context,
+              'github.com',
+              '/manggap/mangapp_public',
             ),
-            padding: const EdgeInsets.all(8),
-            margin: const EdgeInsets.all(8),
-            child: Row(
-              children: [
-                Text(
-                  "V 1.3.0",
-                  style: GoogleFonts.openSans(
-                    color: ColorConstant.whiteColor,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
+            'V 1.3.1',
+            ColorConstant.kThird,
+            ColorConstant.whiteColor,
+            const Icon(
+              Icons.verified,
+              color: ColorConstant.whiteColor,
+            ),
+          ),
+          _buildTapSupportSocial(
+            context,
+            () => _launchChrome('https://saweria.co/ifqygazhar'),
+            'Saweria',
+            const Color(0xFFFAAE31),
+            Colors.black,
+            const Icon(Icons.attach_money),
+          ),
+          _buildTapSupportSocial(
+            context,
+            () => _launchFb(),
+            'Facebook',
+            Colors.blueAccent,
+            ColorConstant.whiteColor,
+            const Icon(
+              Icons.facebook,
+              color: ColorConstant.whiteColor,
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  GestureDetector _buildTapSupportSocial(
+    BuildContext context,
+    Function()? onTap,
+    String title,
+    Color containerColor,
+    Color textColor,
+    Icon icon,
+  ) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: containerColor,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        padding: const EdgeInsets.all(8),
+        margin: const EdgeInsets.only(top: 4, left: 8, right: 8, bottom: 4),
+        child: Row(
+          children: [
+            icon,
+            const SizedBox(
+              width: 8,
+            ),
+            Text(
+              title,
+              style: GoogleFonts.openSans(
+                color: textColor,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
